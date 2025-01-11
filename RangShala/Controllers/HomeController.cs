e3915f4c9b596821cfcffbca5818398dda544eaf
Ayushi0409 <ayushibabariya4@gmail.com>
2025-01-11 08:29:22 +0530

    Add project files.

---
+++ RangShala/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    public ActionResult About()
+    {
+        return View();
+    }
+
+    public ActionResult Artwork()
+    {
+        return View();
+    }
+
+    public ActionResult Artist()
+    {
+        return View();
+    }
+
+    public ActionResult Bespoke()
+    {
+        return View();
+    }
+
+    public ActionResult Join()
+    {
+        return View();
+    }
+
+    public ActionResult Consult()
+    {
+        return View();
+    }
+}
